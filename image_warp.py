6cdf435a9974c42d70b46ecbbabb83d951d5e3f4
WoodMath <jeffwood314@gmail.com>
2016-08-17 23:23:17 -0500

    Updated source files

--- image_warp.py
+++ image_warp.py
@@ -12,15 +12,17 @@ import numpy as np
 sReferences="""
 Taken From:
 	http://scikit-image.org/
+	http://scikit-image.org/docs/dev/auto_examples/applications/plot_geometric.html
 	http://effbot.org/imagingbook/image.htm#tag-Image.Image.transform
 	http://stackoverflow.com/questions/5071063/is-there-a-library-for-image-warping-image-morphing-for-python-with-controlled
 	http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
-    
+	http://www.perrygeo.com/python-affine-transforms.html
+	    
 """
 
 
 im = Image.open('./images/Sport0.png')
-im1 = im.transform(im.size, Image.AFFINE, (1,0,0,0,-im.size[1],0))
-k=im.transform((im.size[0],im.size[1]), Image.AFFINE, (1,0,-50,0,1,-50))
+im1 = im.transform(im.size, Image.AFFINE, (1,0,0,0,1,-im.size[1]))
+k=im.transform((im.size[0],im.size[1]), Image.PERSPECTIVE, (1,0,-50,0,1,-50,0,0))
 #im.show()
 k.show()
